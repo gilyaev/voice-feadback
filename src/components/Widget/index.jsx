5a79eb443827168c5a6885aaf769ca51eedd0b77
Viktor Zhyliaiev <gilyaev@gmail.com>
2018-01-24 10:24:18 +0100

    Stylized step 0-2

--- src/components/Widget/index.jsx
+++ src/components/Widget/index.jsx
@@ -6,13 +6,33 @@ export default class Widget extends Component {
         return (
             <div className={ styles.container }>
                 <div className={ styles.mediaRecorder }>
-                    <p><strong>Is your microphone ready?</strong></p>
-                    <a className={ `${styles.btn}`}>Leave Feedback</a>
-                    <ul className={ styles.steps }>
-                        <li>Record</li>
-                        <li>Listen</li>
-                        <li>Send</li>
-                    </ul>
+                    <div className={ styles.step0 }>
+                        <p><strong>Is your microphone ready?</strong></p>
+                        <a className={`${styles.btn} ${styles.btnMicrophone}`}>Leave Feedback</a>
+                        <ul className={ styles.steps }>
+                            <li>Record</li>
+                            <li>Listen</li>
+                            <li>Send</li>
+                        </ul>
+                    </div>
+                    <div className={ styles.step1 }>
+                        <p><strong>Speak now, your feedback is recording!</strong></p>
+                        <a className={`${styles.btn} ${styles.btnStopRecording}`}>Stop Recording</a>
+                        <ul className={styles.steps}>
+                            <li className={ styles.active }>Record</li>
+                            <li>Listen</li>
+                            <li>Send</li>
+                        </ul>
+                    </div>
+                    <div className={styles.step2}>
+                        <p><strong>Your feeadback is ready to send!</strong></p>
+                        <a className={`${styles.btn} ${styles.btnSendFile}`}>Send Feedback</a>
+                        <ul className={styles.steps}>
+                            <li>Record</li>
+                            <li className={styles.active}>Listen</li>
+                            <li>Send</li>
+                        </ul>
+                    </div>
                 </div>
                 <div className={ styles.avatar }></div>
             </div>
