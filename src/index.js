8d4b7d44b484325eea78443f406d2864e192f5d2
Viktor Zhyliaiev <viktor.zhyliaiev@heroteams.com.au>
2021-12-16 16:23:11 +0200

    Hackethon

--- src/index.js
+++ src/index.js
@@ -4,13 +4,32 @@ import App from '@/components/Widget';
 
 window.onload = function init () {
     const constraints = { audio: true, video: false };
+    let initCb = (app) => {};
+    let autoStart = false;
+    let parentNode = 'body';
+    window[window.g_voicefeedback].q.forEach((item) => {
+        if (item[0] === 'initCb') {
+            initCb = item[1];
+        }
+
+        if (item[0] === 'autoStart') {
+            autoStart = item[1];
+        }
+
+        if (item[0] === 'parentNode') {
+            parentNode = item[1];
+        }
+    });
+
+    // console.log(document.querySelector(parentNode), parentNode);
+
     window.URL = window.URL || window.webkitURL;
     navigator.mediaDevices.getUserMedia(constraints)
         .then(stream => {
             const AudioContext = window.AudioContext;
             const context = new AudioContext();
             const audioInput = context.createMediaStreamSource(stream);
-            render((<App audioSource={ audioInput } />), document.body);
+            window.elmofeedback = render((<App audioSource={ audioInput } fixed={parentNode === 'body'} autoStart={autoStart} initCb={initCb} />), document.querySelector(parentNode));
         })
         .catch(function (err) { console.log(err.name + ': ' + err.message); });
 };
